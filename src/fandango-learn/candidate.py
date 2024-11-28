a9866ef5a6d9efe2cdd2ba05b32f03e165983341
Martin Eberlein <schuldner-01.vordach@icloud.com>
2024-11-28 10:28:39 +0100

    blacked

--- src/fandango-learn/candidate.py
+++ src/fandango-learn/candidate.py
@@ -56,35 +56,3 @@ class FandangoConstraintCandidate(ConstraintCandidate):
 
     def __or__(self, other):
         pass
-
-# class Candidate:
-#     """
-#     A candidate contains a formula, a set of inputs, a list of evaluation results and a combination of inputs and
-#     evaluation results.
-#     """
-#
-#     def __init__(
-#         self,
-#         formula: language.Formula,
-#         inputs: Set[Input] = None,
-#         positive_eval_results: Sequence[bool] = (),
-#         negative_eval_results: Sequence[bool] = (),
-#         comb: Dict[Input, bool] = None,
-#         use_fast_eval: bool = False,
-#         eval_ast: list = None,
-#     ):
-#         """
-#         Initialize a candidate with a formula, a set of inputs, a list of evaluation results and a combination of inputs
-#         and evaluation results.
-#         :param Formula formula: The formula of the candidate.
-#         :param Set[Input] inputs: The inputs of the candidate.
-#         :param Sequence[bool] positive_eval_results: The evaluation results of the candidate on positive inputs.
-#         :param Sequence[bool] negative_eval_results: The evaluation results of the candidate on negative inputs.
-#         :param Dict[Input, bool] comb: The combination of inputs and evaluation results.
-#         """
-#         self.formula = formula
-#         self.inputs = inputs or set()
-#         self.failing_inputs_eval_results: List[bool] = list(positive_eval_results)
-#         self.passing_inputs_eval_results: List[bool] = list(negative_eval_results)
-#         self.comb: Dict[Input, bool] = comb or {}
-#         self.use_fast_eval = use_fast_eval
