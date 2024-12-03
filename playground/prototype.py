d0dc1ab0077ac65bc27d54f6c497389c9b0a34c5
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2024-12-03 15:43:50 +0100

    refinements

--- playground/prototype.py
+++ playground/prototype.py
@@ -5,12 +5,15 @@ from fandangoLearner.input import FandangoInput, OracleResult
 from fandango.language.symbol import NonTerminal
 from fandangoLearner.learner import FandangoLearner
 
+from calculator import calculator_oracle
+
+
 
 if __name__ == "__main__":
     grammar, _ = parse_file("calculator.fan")
     test_inputs = [
         ("sqrt(-900)", OracleResult.FAILING),
-        ("sqrt(-1)", OracleResult.FAILING),
+        ("sqrt(-10)", OracleResult.FAILING),
         ("sin(-900)", OracleResult.PASSING),
         ("sqrt(2)", OracleResult.PASSING),
         ("cos(10)", OracleResult.PASSING),
@@ -19,14 +22,19 @@ if __name__ == "__main__":
     initial_inputs = {
         FandangoInput.from_str(grammar, inp, result) for inp, result in test_inputs
     }
+    for _ in range(10):
+        inp = grammar.fuzz()
+        test_inputs.append((str(inp), calculator_oracle(inp)))
 
-    patterns = [
-        "int(<NON_TERMINAL>) <= <INTEGER>;",
-        "int(<NON_TERMINAL>) == <INTEGER>;",
-        "str(<NON_TERMINAL>) == <STRING>;",
-        "int(<NON_TERMINAL>) == len(str(<NON_TERMINAL>));",
-        "int(<NON_TERMINAL>) == int(<NON_TERMINAL>) * <INTEGER> * int(<NON_TERMINAL>) * <INTEGER>;",
-    ]
+    # initial_inputs = {FandangoInput.from_str(grammar, inp, result) for inp, result in test_inputs}
+
+    # patterns = [
+    #     "int(<NON_TERMINAL>) <= <INTEGER>;",
+    #     "int(<NON_TERMINAL>) == <INTEGER>;",
+    #     "str(<NON_TERMINAL>) == <STRING>;",
+    #     "int(<NON_TERMINAL>) == len(str(<NON_TERMINAL>));",
+    #     "int(<NON_TERMINAL>) == int(<NON_TERMINAL>) * <INTEGER> * int(<NON_TERMINAL>) * <INTEGER>;",
+    # ]
 
     non_terminal_values = {
         NonTerminal("<number>"),
@@ -35,7 +43,7 @@ if __name__ == "__main__":
     }
 
     start_time = time.time()
-    learner = FandangoLearner(grammar, patterns)
+    learner = FandangoLearner(grammar, None)
     end_time = time.time()
     print(f"Time taken to initialize learner: {end_time - start_time:.2f} seconds")
 
@@ -44,9 +52,19 @@ if __name__ == "__main__":
     end_time_learning = time.time()
 
     print(f"Time taken to learn constraints: {end_time_learning - start_time_learning:.4f} seconds")
-    print("Filtered Learned Atomic Constraints:")
+    print("Best Constraints:")
+
+    for _ in range(2000):
+        inp = grammar.fuzz()
+        test_inputs.append((str(inp), calculator_oracle(inp)))
+
+    evaluation_inputs = {FandangoInput.from_str(grammar, inp, result) for inp, result in test_inputs}
+
+    start_time = time.time()
+    print("Evaluate Constraints with: ", len(evaluation_inputs), "inputs")
     for candidate in candidates:
-        candidate.evaluate(initial_inputs)
-        print(
-            f"Constraint: {candidate.constraint}, Recall: {candidate.recall()}, Precision: {candidate.precision()}"
-        )
+        candidate.evaluate(evaluation_inputs)
+        print("Constraint:", candidate.constraint, "Recall:", candidate.recall(), "Precision:", candidate.precision())
+    print("\n")
+
+    print("Time taken to evaluate constraints:", time.time() - start_time)
