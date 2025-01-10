b20cd741f5308a1c6373b80c2c4d4929d3f6be95
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-01-10 14:31:39 +0100

    streamline eval pipeline

--- evaluation/heartbleed/heartbleed.py
+++ evaluation/heartbleed/heartbleed.py
@@ -1,20 +1,24 @@
 import time
-import pathlib
+import os
+import random
 
 from fandango.language.parse import parse_file
 from fandango.language.symbol import NonTerminal
 from fandangoLearner.learner import FandangoLearner
 from fandangoLearner.data.input import FandangoInput
+from fandangoLearner.logger import LoggerLevel
 
-from evaluation.evaluation_helper import evaluate_candidates, print_constraints
-from heartbeat import initial_inputs, oracle_simple as oracle
+from evaluation.heartbleed.heartbeat import initial_inputs as heartbleed_inputs, oracle_simple as oracle
+from evaluation.evaluation_helper import format_results
 
 
-if __name__ == "__main__":
-    grammar, _ = parse_file(pathlib.Path.cwd() / "heartbleed.fan")
+def evaluate_heartbleed(logger_level=LoggerLevel.INFO):
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'heartbleed.fan')
+    grammar, _ = parse_file(filename)
 
     initial_inputs = {
-        FandangoInput.from_str(grammar, inp, oracle(inp)) for inp in initial_inputs
+        FandangoInput.from_str(grammar, inp, oracle(inp)) for inp in heartbleed_inputs
     }
 
     relevant_non_terminals = {
@@ -23,17 +27,26 @@ if __name__ == "__main__":
     }
 
     start_time_learning = time.time()
-    learner = FandangoLearner(grammar)
+    learner = FandangoLearner(grammar, logger_level=logger_level)
 
     candidates = learner.learn_constraints(initial_inputs, relevant_non_terminals)
 
     end_time_learning = time.time()
 
-    print(
-        f"Time taken to learn constraints: {end_time_learning - start_time_learning:.4f} seconds",
-        end="\n\n",
+    # round time
+    time_in_seconds = round(end_time_learning - start_time_learning, 4)
+    return format_results(
+        "Heartbleed",
+        grammar,
+        oracle,
+        candidates,
+        time_in_seconds
     )
 
-    print_constraints(candidates, initial_inputs)
 
-    evaluate_candidates(candidates, grammar, oracle)
+if __name__ == "__main__":
+    random.seed(1)
+    results = evaluate_heartbleed()
+    constraints = results["candidates"]
+    for constraint in constraints:
+        print(constraint)
---
+++ evaluation/run_evaluation.py
@@ -0,0 +1,42 @@
+import random
+from typing import Dict
+
+from evaluation.calculator.evaluate_calculator import evaluate_calculator
+from evaluation.heartbleed.heartbleed import evaluate_heartbleed
+from evaluation.middle.middle import evaluate_middle
+
+from fandangoLearner.logger import LoggerLevel
+
+
+# Return the evaluation results as a tuple of values (subject, total, valid, percentage, diversity, mean_length, median)
+def better_print_results(
+    results: Dict
+):
+    print("================================")
+    print(f"{results["name"]} Evaluation Results")
+    print("================================")
+    print(f"Learned Constraints: {len(results["candidates"])}")
+    print(f"Best Equivalent Constraints: {len(results['best_candidates'])}")
+    print(f"Precision: {results["precision"]:.4f}")
+    print(f"Recall: {results["recall"]:.4f}")
+    print(f"Time: {results['time_in_seconds']:.4f} seconds")
+    print("")
+    print("")
+
+
+def run_evaluation(seconds: int = 3600, random_seed: int = 1):
+    # Set the random seed
+    random.seed(random_seed)
+
+    experiments = [
+        evaluate_calculator,
+        evaluate_heartbleed,
+        evaluate_middle,
+    ]
+
+    for experiment in experiments:
+        better_print_results(experiment(logger_level=LoggerLevel.CRITICAL))
+
+
+if __name__ == "__main__":
+    run_evaluation()
