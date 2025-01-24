188f1b0b6fc33d8d690292def1213e4aa30aae0d
Martin Eberlein <schuldner-01.vordach@icloud.com>
2025-01-24 15:43:46 +0100

    fix generator tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,23 +1,24 @@
 import unittest
-from pathlib import Path
+import os
 
 from fandangoLearner.data.input import FandangoInput
-from fandangoLearner.interface.fandango import parse_file
+from fandangoLearner.interface.fandango import parse
 from fandangoLearner.learning.candidate import FandangoConstraintCandidate
-from fandangoLearner.refinement.generator import FandangoGenerator
+from fandangoLearner.refinement.generator import FandangoGenerator, FandangoGrammarGenerator
 
 
 class TestInputGenerator(unittest.TestCase):
 
     def setUp(self):
-        file = Path("tests/resources/calculator.fan")
-        self.grammar, self.constraints = parse_file(file)
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, "resources", "calculator.fan")
+        self.grammar, self.constraints = parse(filename)
 
-    def test_fandango_fuzzer(self):
+    def test_fandango_generator(self):
         generator = FandangoGenerator(self.grammar)
 
         candidate = FandangoConstraintCandidate(self.constraints[0])
-        test_inputs = generator.generate_test_inputs(candidate)
+        test_inputs = generator.generate_test_inputs(candidate, 10)
 
         self.assertTrue(len(test_inputs) >= 10)
         self.assertTrue(all(isinstance(inp, FandangoInput) for inp in test_inputs))
@@ -25,6 +26,15 @@ class TestInputGenerator(unittest.TestCase):
         for constraint in self.constraints:
             for inp in test_inputs:
                 self.assertTrue(constraint.check(inp.tree))
+
+    def test_fandango_grammar_generator(self):
+        generator = FandangoGrammarGenerator(self.grammar)
+
+        candidate = FandangoConstraintCandidate(self.constraints[0])
+        test_inputs = generator.generate_test_inputs(candidate, 10)
+
+        self.assertTrue(len(test_inputs) == 10)
+        self.assertTrue(all(isinstance(inp, FandangoInput) for inp in test_inputs))
 
 
 if __name__ == "__main__":
