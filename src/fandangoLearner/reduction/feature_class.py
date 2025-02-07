67e3dc2e6ac9b1f5e030f9c6748e3f44d1a06761
Martin Eberlein <schuldner-01.vordach@icloud.com>
2025-02-07 17:08:30 +0100

    correctly handling +,* and ? is a pain

--- src/fandangoLearner/reduction/feature_class.py
+++ src/fandangoLearner/reduction/feature_class.py
@@ -1,18 +1,12 @@
 from typing import List, Set, Dict, Optional, Any
-import re
 from abc import ABC, abstractmethod
-from collections import defaultdict
 
-from isla.language import DerivationTree
-from debugging_framework.input.oracle import OracleResult
-from debugging_framework.fuzzingbook.grammar import (
-    is_nonterminal,
-    reachable_nonterminals,
-)
+from fandango.language.tree import DerivationTree
+from fandango.language.grammar import Grammar, NonTerminalNode, TerminalNode, Node, NonTerminal, Concatenation, Star, \
+    Alternative
 
-from fandango.language.grammar import Grammar, NonTerminalNode, TerminalNode, Terminal, NonTerminal
-from fandangoLearner.reduction.transformer import CleanNameVisitor, DerivableCharsetVisitor, NonTerminalVisitor
-
+from fandangoLearner.reduction.transformer import CleanNameVisitor, DerivableCharsetVisitor, NonTerminalVisitor, ExpansionVisitor
+from fandangoLearner.data.input import OracleResult
 
 class Feature(ABC):
     def __init__(self, non_terminal: NonTerminal):
@@ -21,18 +15,18 @@ class Feature(ABC):
     def __repr__(self) -> str:
         return (
             self._repr()
-            .replace('"', "&quot;")
-            .replace(",", "&comma;")
-            .replace("[", "&lsqb;")
-            .replace("]", "&rsqb;")
-            .replace("{", "&lcub;")
-            .replace("}", "&rcub;")
-            .replace(":", "&colon;")
-            .replace("'", "&#39;")
-            .replace(" ", "&nbsp;")
-            .replace("\n", "&#13;")
-            # .replace("\r", "&#60;")
-            .replace("\\", "")
+            # .replace('"', "&quot;")
+            # .replace(",", "&comma;")
+            # .replace("[", "&lsqb;")
+            # .replace("]", "&rsqb;")
+            # .replace("{", "&lcub;")
+            # .replace("}", "&rcub;")
+            # .replace(":", "&colon;")
+            # .replace("'", "&#39;")
+            # .replace(" ", "&nbsp;")
+            # .replace("\n", "&#13;")
+            # # .replace("\r", "&#60;")
+            # .replace("\\", "")
         )
 
     @abstractmethod
@@ -81,8 +75,11 @@ class ExistenceFeature(Feature):
         return int
 
     def evaluate(self, subtree: DerivationTree) -> int:
-        current_node, _ = subtree
-        return int(self.non_terminal == current_node)
+        # print(subtree.symbol, type(subtree.symbol))
+        # print(self.non_terminal, type(self.non_terminal))
+        # result = int(self.non_terminal == subtree.symbol)
+        # print(result)
+        return int(self.non_terminal == subtree.symbol)
 
     @classmethod
     def factory_method(cls, grammar) -> List[Feature]:
@@ -90,9 +87,10 @@ class ExistenceFeature(Feature):
 
 
 class DerivationFeature(Feature):
-    def __init__(self, non_terminal: NonTerminal, expansion: NonTerminal | Terminal):
+    def __init__(self, non_terminal: NonTerminal, expansion: Node, grammar: Grammar):
         super().__init__(non_terminal)
         self.expansion = expansion
+        self.grammar = grammar
 
     def _repr(self) -> str:
         return f"exists({self.non_terminal} -> {self.expansion})"
@@ -106,25 +104,63 @@ class DerivationFeature(Feature):
         return int
 
     def evaluate(self, subtree: DerivationTree) -> int:
-        current_node, children = subtree
+        """
+        With the given fandango Grammar, evaluating this feature is pretty expensive.
+        We use a couple of checks to speed up the process. If no checks are passed, we parse the subtree with the
+        expansion of the non-terminal and check if the parsed tree exists.
 
-        expansion = "".join([child[0] for child in children])
-        return int(self.non_terminal == current_node and self.expansion == expansion)
+        :param subtree:
+        :return:
+        """
+        current_node = subtree.symbol
+
+        # If the non-terminals don't match, we can immediately return 0
+        if self.non_terminal != current_node:
+            return 0
+
+        # If the production rule for the non-terminal is not an alternative, we can immediately return 1
+        # This is because the expansion of the non-terminal is a single node and it allways has to match the subtree
+        if not isinstance(self.grammar[self.non_terminal], Alternative):
+            return 1
+
+        # If the expansion of the non-terminal is a terminal or non-terminal, we can compare the subtree with the
+        # expansion; ff they match, we can return 1 else 0
+        if isinstance(self.expansion, NonTerminalNode) or isinstance(self.expansion, TerminalNode):
+            # visitor = ExpansionVisitor()
+            children = subtree.children
+            expanded_subtree = " ".join([repr(child.symbol) for child in children])
+            # expanded_expansion = "".join([str(child) for child in visitor.visit(self.expansion)])
+            expanded_expansion = repr(self.expansion.symbol)
+            if expanded_expansion == expanded_subtree:
+                return 1
+            return 0
+
+        # If the expansion is an Alternative and consists not of trivial NonTerminal or Terminal nodes, we need to parse
+        # the subtree with the expansion of the non-terminal and check if the parsed tree exists.
+        new_rules = self.grammar.rules.copy()
+        new_rules[self.non_terminal] = self.expansion
+        parsed = Grammar.Parser(new_rules).parse(str(subtree), start=self.non_terminal)
+        if parsed:
+            return 1
+
+
+        # visitor = ExpansionVisitor()
+        # children = subtree.children
+        # expanded_subtree = " ".join([repr(child.symbol) for child in children])
+        # expanded_expansion = "".join([str(child) for child in visitor.visit(self.expansion)])
+        # print(expanded_subtree, expanded_expansion)
+        #
+        # return int(self.non_terminal == current_node and expanded_subtree == expanded_expansion)
+        return 0
 
     @classmethod
     def factory_method(cls, grammar: Grammar) -> List[Feature]:
         features = []
-        clean_name_visitor = CleanNameVisitor()
+        visitor = ExpansionVisitor()
 
-        for non_terminal in grammar:
-            node = grammar.rules[non_terminal]
-            if isinstance(node, NonTerminalNode):
-                features.append(cls(non_terminal, node.symbol))
-            elif isinstance(node, TerminalNode):
-                features.append(cls(non_terminal, node.symbol))
-            else:
-                for expansion in grammar.rules[non_terminal].descendents(grammar.rules):
-                    features.append(cls(non_terminal, expansion.symbol))
+        for non_terminal, node in grammar.rules.items():
+            for expansion in visitor.visit(node):
+                features.append(cls(non_terminal, expansion, grammar))
 
         return features
 
@@ -143,55 +179,68 @@ class NumericFeature(Feature):
 
     def evaluate(self, subtree: DerivationTree) -> Any:
         try:
-            value = float(tree_to_string(subtree))
+            value = float(str(subtree))
             return value
         except ValueError:
             return self.default_value
 
     @classmethod
+    def get_reachable_non_terminals(cls, grammar: Grammar, non_terminal: NonTerminal) -> Set[NonTerminal]:
+        reachable = set()
+        non_terminal_visitor = NonTerminalVisitor()
+
+        def _find_reachable_nonterminals(grammar_: Grammar, symbol: NonTerminal):
+            nonlocal reachable
+            reachable.add(symbol)
+            expansion_node = grammar_.rules.get(symbol, [])
+            for exp in non_terminal_visitor.visit(expansion_node):
+                if exp not in reachable:
+                    _find_reachable_nonterminals(grammar_, exp)
+
+        _find_reachable_nonterminals(grammar, non_terminal)
+        return reachable
+
+
+    @classmethod
     def factory_method(cls, grammar) -> List[Feature]:
-        v = DerivableCharsetVisitor()
-        derivable_chars: dict[NonTerminal, set[str]] = dict()
-        for non_terminal in grammar:
-            node = grammar.rules[non_terminal]
-            derivable_chars[non_terminal] = v.visit(node)
-
-        return cls.get_features(grammar, derivable_chars)
-
-    @classmethod
-    def is_numeric(cls, non_terminal: NonTerminal, derivable_chars: dict[NonTerminal, set[str]]) -> bool:
-        numeric_chars = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
-        numeric_symbols = {".", "-"}
-
-        chars = derivable_chars[non_terminal]
-        non_numeric_chars = (chars - numeric_chars) - numeric_symbols
-        has_numeric_chars = len(chars.intersection(numeric_chars)) > 0
-        print(chars)
-        if len(non_numeric_chars) == 0 and has_numeric_chars:
-            return True
-        return False
-
-    @classmethod
-    def get_features(cls, grammar: Grammar, derivable_chars: Dict[NonTerminal, Set[str]]) -> List[Feature]:
         """
-        Gets a list of NumericInterpretation features for each rule that derives only numeric characters.
+        Get all derivable chars for all non-terminals in the grammar.
+        Get all non-terminals that are reachable for a given non-terminal.
 
-        :return: A list of NumericInterpretation features.
+        Check if for a non-terminal all derivable chars for all reachable non-terminals are numeric.
+
+        :param grammar:
+        :return:
         """
         features = []
-        numeric_chars = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
-        numeric_symbols = {".", "-"}
+        derivable_char_visitor = DerivableCharsetVisitor()
+        derivable_chars: dict[NonTerminal, set[str]] = dict()
 
-        non_term_visitor = NonTerminalVisitor()
+        # Get all derivable chars for all non-terminals in the grammar
+        for non_terminal, node in grammar.rules.items():
+            derivable_chars[non_terminal] = derivable_char_visitor.visit(node)
 
-        for non_terminal in grammar:
-            children = non_term_visitor.visit(grammar.rules[non_terminal])
-            print(non_terminal, children)
-
-            if all(cls.is_numeric(non_terminal, derivable_chars) for non_terminal in children):
+        # Check if for a non-terminal all derivable chars for all reachable non-terminals are numeric
+        for non_terminal, node in grammar.rules.items():
+            reachable_non_terminals = cls.get_reachable_non_terminals(grammar, non_terminal)
+            reachable_chars = set()
+            for reachable_non_terminal in reachable_non_terminals:
+                reachable_chars.update(derivable_chars[reachable_non_terminal])
+            if cls.is_numeric(reachable_chars):
                 features.append(cls(non_terminal))
 
         return features
+
+    @classmethod
+    def is_numeric(cls, char_set: set[str]) -> bool:
+        numeric_chars = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+        numeric_symbols = {".", "-"}
+
+        non_numeric_chars = (char_set - numeric_chars) - numeric_symbols
+        has_numeric_chars = len(char_set.intersection(numeric_chars)) > 0
+        if len(non_numeric_chars) == 0 and has_numeric_chars:
+            return True
+        return False
 
 
 class LengthFeature(Feature):
@@ -206,7 +255,7 @@ class LengthFeature(Feature):
         return int
 
     def evaluate(self, subtree: DerivationTree) -> Any:
-        return len(tree_to_string(subtree))
+        return len(str(subtree))
 
     @classmethod
     def factory_method(cls, grammar) -> List[Feature]:
@@ -262,11 +311,3 @@ class FeatureVector:
 
     def __repr__(self):
         return f"{self.test_input}: {self.features}"
-
-
-def tree_to_string(tree: DerivationTree) -> str:
-    symbol, children, *_ = tree
-    if children:
-        return "".join(tree_to_string(c) for c in children)
-    else:
-        return "" if is_nonterminal(symbol) else symbol
