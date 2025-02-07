29a1612113779dfece9874470f5d5f2c4ad20445
Martin Eberlein <schuldner-01.vordach@icloud.com>
2025-02-07 17:08:44 +0100

    fixed feature collector

--- src/fandangoLearner/reduction/feature_collector.py
+++ src/fandangoLearner/reduction/feature_collector.py
@@ -2,19 +2,24 @@ from functools import lru_cache
 from typing import List, Dict, Optional, Any, Type
 from abc import ABC, abstractmethod
 
-from debugging_framework.fuzzingbook.grammar import is_nonterminal, Grammar
-from isla.language import DerivationTree
+# from debugging_framework.fuzzingbook.grammar import is_nonterminal, Grammar
+# from isla.language import DerivationTree
 
-from ..data.input_data import Input
-from .features import (
+from fandango.language.grammar import Grammar
+from fandango.language.symbol import NonTerminal, Symbol
+from fandango.language.tree import DerivationTree
+
+from fandangoLearner.data.input import FandangoInput
+from fandangoLearner.reduction.feature_class import (
+    Feature,
     ExistenceFeature,
     DerivationFeature,
     NumericFeature,
     LengthFeature,
-    Feature,
-    FeatureVector,
     FeatureFactory,
+    FeatureVector,
 )
+
 
 DEFAULT_FEATURE_TYPES: List[Type[Feature]] = [
     ExistenceFeature,
@@ -37,12 +42,12 @@ class FeatureCollector(ABC):
         return factory.build(feature_types)
 
     @abstractmethod
-    def collect_features(self, test_input: Input) -> Dict[str, Any]:
+    def collect_features(self, test_input: FandangoInput) -> Dict[str, Any]:
         pass
 
 
 class GrammarFeatureCollector(FeatureCollector):
-    def collect_features(self, test_input: Input) -> FeatureVector:
+    def collect_features(self, test_input: FandangoInput) -> FeatureVector:
         feature_vector = FeatureVector(str(test_input))
 
         for feature in self.features:
@@ -52,7 +57,9 @@ class GrammarFeatureCollector(FeatureCollector):
         return feature_vector
 
     def set_features(self, tree: DerivationTree, feature_vector: FeatureVector):
-        (node, children) = tree
+        node: Symbol = tree.symbol
+        children = tree.children
+        assert isinstance(node, NonTerminal)
 
         corresponding_features_1d = self.get_corresponding_feature(node)
 
@@ -61,11 +68,11 @@ class GrammarFeatureCollector(FeatureCollector):
             feature_vector.set_feature(corresponding_feature, value)
 
         for child in children:
-            if is_nonterminal(child[0]):
+            if isinstance(child.symbol, NonTerminal):
                 self.set_features(child, feature_vector)
 
     @lru_cache
-    def get_corresponding_feature(self, current_node: str) -> List[Feature]:
+    def get_corresponding_feature(self, current_node: NonTerminal) -> List[Feature]:
         return [
             feature
             for feature in self.features
