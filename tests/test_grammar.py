a1c2cc45400b650318d03faeddf20cb4fca4d89f
Martin Eberlein <schuldner-01.vordach@icloud.com>
2025-02-07 19:44:14 +0100

    fix grammar test

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -1,16 +1,8 @@
 import unittest
-from pathlib import Path
 import os
 
-from fandango.constraints.base import DisjunctionConstraint
-from fandango.language.symbol import NonTerminal
-
-from fandangoLearner.data.input import FandangoInput
-from fandangoLearner.interface.fandango import parse_file, parse_constraint, parse
-from fandangoLearner.learning.candidate import FandangoConstraintCandidate
-from fandangoLearner.learning.combination import ConjunctionProcessor, DisjunctionProcessor
-from fandangoLearner.learning.candidate import CandidateSet
-from fandangoLearner.reduction.transformer import NameGrammarVisitor
+from fandangoLearner.interface.fandango import parse
+from fandangoLearner.reduction.transformer import FuzzingBookGrammarTransformer
 
 
 class TestConjunctionProcessor(unittest.TestCase):
@@ -21,11 +13,7 @@ class TestConjunctionProcessor(unittest.TestCase):
 
 
     def test_grammar_parsing(self):
-
-        start_node = self.grammar.rules[NonTerminal("<arithexp>")]
-
-        visitor = NameGrammarVisitor()
-        #  print(visitor.visit(start_node))
+        visitor = FuzzingBookGrammarTransformer()
 
         for non_term, node in self.grammar.rules.items():
             expansions = visitor.visit(node)
