63b7f5839f1b97869577fa6a53a6ec9cf7a5b551
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-02-10 13:33:35 +0100

    feat: add version number to fandango-learn

--- src/fdlearn/__init__.py
+++ src/fdlearn/__init__.py
@@ -1,9 +1,2 @@
-import importlib.resources as pkg_resources
+__version__ = "0.9.0"
 
-
-def get_pattern_file_path():
-    return pkg_resources.files("fandango-learn.resources") / "patterns.toml"
-
-
-def get_islearn_pattern_file_path():
-    return pkg_resources.files("fandango-learn.resources") / "patterns_islearn.toml"
