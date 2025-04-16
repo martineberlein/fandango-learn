ae63269088131c9cd45021bb1e0214c3efd5ab37
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-04-16 14:23:32 +0200

    Added new __contains__ function overwriting the default contains method. Adds recursive contains checking

--- src/fdlearn/__init__.py
+++ src/fdlearn/__init__.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+from fandango.language.tree import DerivationTree
 
 __version__ = "0.9.1"
 
+
+def __contains__(self, other) -> bool:
+    if isinstance(other, DerivationTree):
+        if self.find_all_nodes(other.symbol):
+            return True
+        return False
+    return other in self.value()
+
+
+DerivationTree.__contains__ = __contains__
