630a44d1e8123c8605b6656b0335850af6c0d763
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-06-25 13:15:09 +0200

    added generator interface test

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 
+from fandango.evolution.algorithm import Fandango
 from fandango.language.grammar import Grammar
 from fandango.constraints.base import Constraint
 
@@ -10,13 +11,8 @@ from fdlearn.interface.fandango import parse, parse_constraint, parse_contents
 class InterfaceTest(unittest.TestCase):
 
     GRAMMAR = """
-    <start> ::= <ab>;
-    <ab> ::= 
-          "a" <ab> 
-        | <ab> "b"
-        | ""
-        ;
-    """
+<start> ::= <ab>;
+<ab> ::= "a" <ab> | <ab> "b"| "" ;"""
 
     def test_parse(self):
         """
@@ -35,7 +31,7 @@ class InterfaceTest(unittest.TestCase):
         """
         Test the parse contents function.
         """
-        grammar, constraints = parse_contents(self.GRAMMAR + "str(<ab>) == 'a';")
+        grammar, constraints = parse_contents(self.GRAMMAR + "where str(<ab>) == 'a'")
         self.assertIsInstance(grammar, Grammar)
 
         self.assertIsNotNone(constraints)
@@ -46,7 +42,7 @@ class InterfaceTest(unittest.TestCase):
         """
         Test the parse_constraint function.
         """
-        constraint = parse_constraint("str(<ab>) == 'a';")
+        constraint = parse_constraint("where str(<ab>) == 'a'")
         self.assertIsInstance(constraint, Constraint)
 
     def test_parse_constraint_none(self):
@@ -55,13 +51,32 @@ class InterfaceTest(unittest.TestCase):
         This should return False. If this tests fails, it means that the fandango check function is broken.
         See fandango issue #346.
         """
-        constraint = parse_constraint("int(<start>) == int(<ab>);")
+        constraint = parse_constraint("where int(<start>) == int(<ab>)")
         self.assertIsInstance(constraint, Constraint)
 
         grammar, _ = parse_contents(self.GRAMMAR)
         tree = grammar.parse("a")
         self.assertIsNotNone(tree)
         self.assertFalse(constraint.check(tree))
+
+    def test_fandango_fuzzer(self):
+        """
+        Test the fandango fuzzer.
+        """
+        grammar, _ = parse_contents(self.GRAMMAR)
+        constraint = parse_constraint("where len(<start>) > 0")
+        self.assertIsInstance(grammar, Grammar)
+        solutions = []
+
+        fandango = Fandango(grammar, [constraint])
+        fan_gen = fandango.generate()
+
+        for solution in fan_gen:
+            solutions.append(str(solution))
+            if len(solutions) == 2:
+                break
+
+        self.assertEqual(len(solutions), 2)
 
 
 if __name__ == "__main__":
