a32648f6ed6efe1695a56fb89b02432502082843
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-10-06 17:15:57 +0200

    add rule induction functionality with example usage

---
+++ doc/rule_induction.py
@@ -0,0 +1,53 @@
+from fandango.language.parse import parse
+from fandango.language.symbol import NonTerminal
+
+from fdlearn.resources.patterns import Pattern
+from fdlearn.learning.candidate import FandangoConstraintCandidate
+from fdlearn.data.input import FandangoInput
+from fdlearn.learning.rule_induction.rule_induction import RuleInductionLearner
+from fdlearn.interface.fandango import parse_contents
+
+grammar = """
+<start> ::= <arithexp>;
+<arithexp> ::= <function>"("<number>")";
+<function> ::= "sqrt" | "cos" | "sin" | "tan";
+<number> ::= <maybeminus><onenine><maybedigits> | "0";
+<maybeminus> ::= "-" | "";
+<onenine> ::= "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+<maybedigits> ::= <digit>*;
+<digit>::=  "0" | <onenine>;
+"""
+
+
+if __name__ == "__main__":
+    grammar, _ = parse_contents(grammar)
+
+    valid_inputs = {
+            FandangoInput.from_str(grammar, "sqrt(-12)", True),
+            FandangoInput.from_str(grammar, "sqrt(-900)", True),
+            FandangoInput.from_str(grammar, "sqrt(-2)", True),
+            FandangoInput.from_str(grammar, "sqrt(-11234)", True),
+            FandangoInput.from_str(grammar, "sqrt(12)", False),
+            FandangoInput.from_str(grammar, "sqrt(0)", False),
+            FandangoInput.from_str(grammar, "cos(-12)", False),
+        }
+
+    relevant_non_terminal = {
+        NonTerminal("<number>"),
+        NonTerminal("<maybeminus>"),
+        NonTerminal("<function>"),
+    }
+
+    from time import time
+
+    start_time = time()
+
+    learner = RuleInductionLearner(
+        grammar,
+    )
+    invariants = learner.learn_constraints(valid_inputs, relevant_non_terminals=relevant_non_terminal)
+
+    print(time() - start_time)
+
+    print(invariants)
+
